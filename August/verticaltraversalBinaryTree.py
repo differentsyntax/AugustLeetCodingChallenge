cd0da786387946b0e0e43d5bd8ed90b464d86c4d
differentsyntax <nothingreallyfaraway@gmail.com>
2020-08-08 09:10:32 -0400

    Vertical order binary tree traversal

--- August/verticaltraversalBinaryTree.py
+++ August/verticaltraversalBinaryTree.py
@@ -9,39 +9,51 @@ class Solution:
         
         outPut = {}
         x = 0
+        y = 0
         main = []
         
-        outPut[x] = [root.val]
+        outPut[(x,y)] = [root.val]
         
-        self.helper(root, outPut, x, main)
+        self.helper(root, outPut, x, y)
+        
+        xMapy = {}
+        left = []
+            
+        for key in outPut:
+            if key[0] not in left:
+                left.append(key[0])
+            if key[0] in xMapy:
+                xMapy[key[0]].append(key[1])
+            else:
+                xMapy[key[0]] = [key[1]]
+            
+            xMapy[key[0]].sort(reverse=True)
+        
+        left.sort()
+        
+        for l in left:
+            temp = []
+            for r in xMapy[l]:
+                temp.extend(outPut[(l, r)])
+            main.append(temp)
         
         return(main)
         
-    def helper(self, node, outPut, x, main):
+    def helper(self, node, outPut, x, y):
         
-        if not (node.left or node.right):
-            return True
-         
         if node.left:
-            if x-1 in outPut:
-                outPut[x-1].append(node.left.val)
+            if (x-1, y-1) in outPut:
+                outPut[(x-1, y-1)].append(node.left.val)
             else:
-                outPut[x-1] = [node.left.val]
-            self.helper(node.left, outPut, x-1, main)
+                outPut[(x-1, y-1)] = [node.left.val]
+            self.helper(node.left, outPut, x-1, y-1)
     
         if node.right:
-            if x+1 in outPut:
-                outPut[x+1].append(node.right.val)
+            if (x+1, y-1) in outPut:
+                outPut[(x+1, y-1)].append(node.right.val)
             else:
-                outPut[x+1] = [node.right.val]
-            self.helper(node.right, outPut, x+1, main)
+                outPut[(x+1, y-1)] = [node.right.val]
+            self.helper(node.right, outPut, x+1, y-1)
             
-        toSort = []
-            
-        for key in outPut:
-            toSort.append(key)
-
-        toSort.sort()
-
-        for x in toSort:
-            main.append(outPut[x])
+        if not (node.left or node.right):
+            return outPut
